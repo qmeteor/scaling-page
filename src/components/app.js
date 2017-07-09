980d5e91abc7414fec06a6d675675669f907143a
Bien Pham <pham_bv@hotmail.com>
2017-07-09 16:29:06 -0400

    added landing page

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Info from './info';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Info/>
+      </div>
     );
   }
 }
---
+++ src/components/info.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Bien on 2017-07-09.
+ */
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+
+export default class Info extends Component {
+    render() {
+        return (
+          <div>
+          </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+
+}
+
